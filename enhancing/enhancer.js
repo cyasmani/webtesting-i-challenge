94b270e2a557f2a4efe43122b8a41124add91aed
cyasmani <yasmanicastaneda@yahoo.com>
2020-11-30 19:24:35 -0800

    Completed mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,33 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement <= 19) {
+    return item.enhancement = item.enhancement + 1
+  } else if(item.enhancement === 20) {
+    return { ...item }
+  }
+
+  //return { ...item };
 }
 
 function fail(item) {
+ if(item.enhancement < 15) {
+  return item.durability = item.durability - 5
+  } else if(item.enhancement >= 15 || item.enhancement > 16) {
+      if(item.enhancement >= 15) {
+        return item.durability = item.durability - 10
+      } else if(item.enhancement > 16) {
+        return item.enhancement = item.enhancement - 1
+      }
+  } 
+
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  item.durability = 100
+ 
+  return { ...item};
 }
 
 function get(item) {
